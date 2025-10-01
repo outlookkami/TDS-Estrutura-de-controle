46901cdd614fc0091623444cec5de9f3a63a5b02
unknown <dkamillythauanny@gmail.com>
2025-10-01 08:51:01 -0300

    Estrutura de controle

---
+++ script.js
@@ -0,0 +1,10 @@
+function VerificarIdade(){
+    idade = document.getElementById('idade').value;
+    resultado = document.getElementById('resultado').innerHTML;
+
+    if (idade>18){
+        resultado.innerHTML('Você é maior de idade');
+    }else{
+        resultado.innerHTML('Você é menor de idade');
+    }
+}
