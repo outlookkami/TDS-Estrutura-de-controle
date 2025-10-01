dc3cf32a8396a28a8ffa34628e1f1649aae05f31
unknown <dkamillythauanny@gmail.com>
2025-10-01 11:36:33 -0300

    continuando css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function VerificarIdade(){
     idade = document.getElementById('idade').value;
-    resultado = document.getElementById('resultado').innerHTML;
+    resultado = document.getElementById('resultado').innerHTML('');
 
     if (idade>18){
         resultado.innerHTML('Você é maior de idade');
